3177596c224b1602b65a9ee6e3d8d17d51db8ff6
AfiqRazak <afiqrazak380@gmail.com>
2023-11-09 23:21:04 +0000

    Adding style to h2 elements

--- conditionalRender/src/UserGreeting.jsx
+++ conditionalRender/src/UserGreeting.jsx
@@ -3,8 +3,8 @@
 function UserGreeting(props){
 
 
-  return (props.isLogedIn ? <h2>Welcome {props.username}</h2> 
-                          : <h2>Please {props.username} login to continue</h2>); 
+  return (props.isLogedIn ? <h2 className="welcome-massage">Welcome back {props.username}</h2> 
+                          : <h2 className="login-prompt">Please login to continue</h2>); 
   
   // if(props.isLogedin){
   //   return <h2>Welcome {props.username}</h2>
